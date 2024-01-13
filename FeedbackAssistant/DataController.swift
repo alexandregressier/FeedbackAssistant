c5799720f47e9994c1617a8ad71fe816162c11d4
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 19:30:53 +0100

    Load the persistent stores

--- FeedbackAssistant/DataController.swift
+++ FeedbackAssistant/DataController.swift
@@ -1,1 +1,18 @@
 import CoreData
+
+class DataController: ObservableObject {
+    let container: NSPersistentCloudKitContainer
+ 
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Main")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
+        }
+        container.loadPersistentStores { storeDescription, error in
+            if let error {
+                fatalError("Fatal error loading store: \(error.localizedDescription)")
+            }
+        }
+    }
+}
