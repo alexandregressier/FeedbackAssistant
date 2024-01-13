0a12a233a0711f4553576e41f86e3650e5635bdc
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 22:01:21 +0100

    Complete the Filter struct

--- FeedbackAssistant/Filter.swift
+++ FeedbackAssistant/Filter.swift
@@ -6,4 +6,24 @@ struct Filter: Identifiable, Hashable {
     var icon: String
     var minModificationDate = Date.distantPast
     var tag: Tag?
+    
+    static var all = Filter(
+        id: UUID(),
+        name: "All Issues",
+        icon: "tray"
+    )
+    static var recent = Filter(
+        id: UUID(),
+        name: "Recent issues",
+        icon: "clock",
+        minModificationDate: .now.addingTimeInterval(-7 * 86400) // 7 days ago
+    )
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    static func ==(lhs: Filter, rhs: Filter) -> Bool {
+        lhs.id == rhs.id
+    }
 }
