66fad8397d359d1c6e8397d4e7ab18fbc30e4246
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 17:09:30 +0100

    Initial revision of Feedback Assistant

---
+++ FeedbackAssistant/FeedbackAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackAssistantApp.swift
+//  FeedbackAssistant
+//
+//  Created by Alex on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
