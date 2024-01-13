98f10b7e9a97af44d62e393ae800855ae7180564
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 20:09:45 +0100

    Instantiate DataController

--- FeedbackAssistant/FeedbackAssistantApp.swift
+++ FeedbackAssistant/FeedbackAssistantApp.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 @main
 struct FeedbackAssistantApp: App {
+    @StateObject var dataController =  DataController() // Stays alive for the whole duration of the app
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext) // Allows any child view to access CoreData with `@Environment(\.managedObjectContext) var managedObjectContext`
+                .environmentObject(dataController)
         }
     }
 }
