15cc2180e41937b75e291ff918009ecbb13af1bf
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 21:53:19 +0100

    Create the Filter struct

---
+++ FeedbackAssistant/Filter.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Filter: Identifiable, Hashable {
+    var id: UUID
+    var name: String
+    var icon: String
+    var minModificationDate = Date.distantPast
+    var tag: Tag?
+}
