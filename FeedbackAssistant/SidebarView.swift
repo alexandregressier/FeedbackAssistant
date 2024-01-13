08417f191a6e78cf5e734d0d884c362663984da3
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 22:55:35 +0100

    Implement the Tags section

--- FeedbackAssistant/SidebarView.swift
+++ FeedbackAssistant/SidebarView.swift
@@ -2,7 +2,21 @@ import SwiftUI
 
 struct SidebarView: View {
     @EnvironmentObject var dataController: DataController
+    
     let smartFilters: [Filter] = [.all, .recent]
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+    
+    var tagFilters: [Filter] {
+        tags.map {
+            Filter(
+                id: $0.id ?? UUID(),
+                name: $0.name ?? "Unnamed",
+                icon: "tag",
+                tag: $0
+            )
+        }
+    }
     
     var body: some View {
         List(selection: $dataController.selectedFilter) {
@@ -12,6 +26,21 @@ struct SidebarView: View {
                         Label(filter.name, systemImage: filter.icon)
                     }
                 }
+            }
+            Section("Tags") {
+                ForEach(tagFilters) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            Button {
+                dataController.deleteAll()
+                dataController.createSampleData()
+            } label: {
+                Label("Add Samples", systemImage: "flame")
             }
         }
     }
