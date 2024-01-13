7670058ff5a0fc81fce04a94bc0222445dda0ce5
Alexandre Gressier <alexandre@gressier.dev>
2024-01-13 21:39:03 +0100

    Create the 3 main views

---
+++ FeedbackAssistant/DetailView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ FeedbackAssistant/SidebarView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text("Sidebar")
+    }
+}
+
+#Preview {
+    SidebarView()
+}
